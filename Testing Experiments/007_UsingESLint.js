4480f7123b9633bfbb7f67cbe2f52ad88bf5be1a
zacaree <zacharem@gmail.com>
2019-05-15 16:22:05 -0500

    Ready for lesson 15

---
+++ Testing Experiments/007_UsingESLint.js
@@ -0,0 +1,12 @@
+// File full of probs. ES Lint to the rescue!
+const name = 'Dan';
+typeof name === 'strng';
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser :(
+}
+
+const greeting = 'hello';
+console.log('${greting} world!');
+
+[(1, 2, 3)].forEach(x => console.log(x));

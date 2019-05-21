3f493747a8d86023a2cb8d3d52f5db5d5817161b
Zac <zacaree@gmail.com>
2019-05-21 07:58:46 -0500

    Playing with a jest mock

--- ReactTestingCourse-ScottTolinski/Level-Up-JavaScript-Testing-101/src/App.test.js
+++ ReactTestingCourse-ScottTolinski/Level-Up-JavaScript-Testing-101/src/App.test.js
@@ -1,4 +1,9 @@
 import { total } from './App';
+import { add } from './add';
+
+jest.mock('./add', () => ({
+  add: jest.fn(() => 35)
+}));
 
 // Integration test
 // It's an integration test because total relies on add in order to function correctly.

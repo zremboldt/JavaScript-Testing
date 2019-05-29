6ed31cf6718491db4ce48994adf5ce9333f42a3a
zacaree <zacharem@gmail.com>
2019-05-29 14:38:25 -0500

    Finished tests for <Counter />

--- ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/Counter.js
+++ ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/Counter.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+  };
+
+  handleCount = () => {
+    // This is now the recommended way to update state. Use prevState.
+    this.setState(prevState => ({
+      count: prevState.count + 1,
+    }));
   };
 
   render() {
     return (
       <div>
-        <button data-testid="counter-button">{this.state.count}</button>
+        <button data-testid="counter-button" onClick={this.handleCount}>
+          {this.state.count}
+        </button>
       </div>
     );
   }
--- ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/Counter.test.js
+++ ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/Counter.test.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import Counter from './Counter';
+
+afterEach(cleanup);
 
 test('<Counter />', () => {
   const { debug, getByTestId } = render(<Counter />);
-  debug(); // outputs Counter dom elements in the terminal
+  const counterButton = getByTestId('counter-button');
+
+  // outputs Counter dom element in the terminal
+  // debug();
+
   // Assert that counter-button is a button
-  expect(getByTestId('counter-button').tagName).toBe('BUTTON');
+  expect(counterButton.tagName).toBe('BUTTON');
   // Assert that counter-button starts at 0
-  expect(getByTestId('counter-button').textContent).toBe('0');
+  expect(counterButton.textContent).toBe('0');
+
+  // fireEvent lets you simulate an event occuring in the DOM.
+  fireEvent.click(counterButton);
+  expect(counterButton.textContent).toBe('1');
+
+  fireEvent.click(counterButton);
+  expect(counterButton.textContent).toBe('2');
+
+  // outputs current Counter dom element in the terminal
+  // debug();
 });

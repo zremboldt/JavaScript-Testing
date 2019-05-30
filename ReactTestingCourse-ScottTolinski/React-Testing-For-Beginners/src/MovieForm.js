351d184d1a29c25781fe340f02813aaad93741a9
zacaree <zacharem@gmail.com>
2019-05-29 21:21:33 -0500

    Using spies

--- ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/MovieForm.js
+++ ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/MovieForm.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 export default class MovieForm extends Component {
+  state = {
+    text: ''
+  };
+
   render() {
+    const { text } = this.state;
+    const { handleSubmit } = this.props;
+
     return (
-      <form data-testid="movie-form">
+      <form data-testid="movie-form" onSubmit={() => handleSubmit({ text })}>
         <input type="text" />
         <button type="submit">Submit</button>
       </form>
--- ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/MovieForm.test.js
+++ ReactTestingCourse-ScottTolinski/React-Testing-For-Beginners/src/MovieForm.test.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from 'react-testing-library';
-import NewMovie from './NewMovie';
+import MovieForm from './MovieForm';
 
 afterEach(cleanup);
 
-test('<NewMovie />', () => {
-  const { debug, getByTestId, queryByTestId, container, getByText } = render(<NewMovie />);
+const onSubmit = jest.fn();
 
-  debug();
+test('<MovieForm />', () => {
+  const { queryByTestId, getByText } = render(<MovieForm handleSubmit={onSubmit} />);
 
-  expect(getByTestId('page-title').textContent).toBe('New Movie');
+  // debug();
 
   // we're expecting the movie-form component to exist.
   // getBy and queryBy give us different results if the component isn't there.
   expect(queryByTestId('movie-form')).toBeTruthy();
 
-  expect(container.firstChild).toMatchSnapshot();
-
   // console.log(container.firstChild);
+
+  fireEvent.click(getByText('Submit'));
+  expect(onSubmit).toHaveBeenCalledTimes(1);
 });
